116ee64acb9fac0f89c58b0b0c580079324a661e
KGreenhorn03 <kylegreenhorn@hotmail.co.uk>
2023-02-07 11:02:06 +0000

    Brushing up the final build.

--- Karting Chaos/Assets/Scripts/CarController.cs
+++ Karting Chaos/Assets/Scripts/CarController.cs
@@ -16,16 +16,15 @@ public class CarController : MonoBehaviour
     private Vector3 MoveForce;
 
     // Update is called once per frame.
-    private void Update()
+    private void FixedUpdate()
     {
 
         //This creates forwards and backwards movement.
-        MoveForce += transform.forward * MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.position += MoveForce * Time.deltaTime;
+        MoveForce += transform.forward * MoveSpeed * Input.GetAxis("Vertical") * Time.fixedDeltaTime;
 
         //This creates the steering in-game.
         float steerInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * steerInput * MoveForce.magnitude * SteerAngle * Time.deltaTime);
+        transform.Rotate(Vector3.up * steerInput * MoveForce.magnitude * SteerAngle * Time.fixedDeltaTime);
 
         //This is the drag and max speed limit.
         MoveForce *= Drag;
@@ -34,7 +33,11 @@ public class CarController : MonoBehaviour
         //This is for the traction in-game.
         Debug.DrawRay(transform.position, MoveForce.normalized * 3);
         Debug.DrawRay(transform.position, transform.forward * 3, Color.blue);
-        MoveForce = Vector3.Lerp(MoveForce.normalized, transform.forward, Traction * Time.deltaTime) * MoveForce.magnitude;
+        MoveForce = Vector3.Lerp(MoveForce.normalized, transform.forward, Traction * Time.fixedDeltaTime) * MoveForce.magnitude;
+
+
+        // Apply movement
+        transform.position += MoveForce * Time.fixedDeltaTime;
     }
 
 }
